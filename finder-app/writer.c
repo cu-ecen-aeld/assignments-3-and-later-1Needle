5f024e9ea603ad214b7780b49c7f97367345c433
1Needle <tmpss99170@gmail.com>
2024-06-08 23:03:22 +0800

    fix writer

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -1,17 +1,30 @@
-#include <fcntl.h>
 #include <syslog.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
-int main(int argc, char **argv)
-{
-	openlog(NULL, 0, LOG_USER);
-	if(argc != 3)
-	{
-		syslog(LOG_ERR, "Invalid parameters");
-		return 1;
-	}
-	int fd = open(argv[1], O_WRONLY | O_CREAT, S_IRWXU | S_IRGRP | S_IROTH);
-	write(fd, argv[2], sizeof(char) * strlen(argv[2]));
-	syslog(LOG_DEBUG, "Writing %s to %s", argv[2], argv[1]);
-	return 0;
+int main(int argc, char *argv[]){
+
+    openlog("writer.c", LOG_PID, LOG_USER);
+    if( argc != 3){
+        syslog(LOG_ERR,"Illegal number of parameters");
+        closelog();
+        return 1;
+    }
+    
+    // assume the directory is created by the caller
+    int bytes = strlen(argv[2]);
+    const char *file_path = argv[1];
+    int fd = open(file_path, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+    if(write(fd, argv[2], bytes) != bytes){
+        syslog(LOG_ERR,"Truncated write");
+        closelog();
+        return 1;
+    }
+    
+    syslog(LOG_INFO, "Writing %s to %s", argv[2], argv[1]);
+    closelog();
+    return 0;
+
 }
